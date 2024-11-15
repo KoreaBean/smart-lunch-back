a1088d4f02c4d3b6cfb102849a78ad0304298d84
KoreaBean <155369309+KoreaBean@users.noreply.github.com>
2024-11-15 17:22:32 +0900

    init

---
+++ lunch-back/src/main/java/hello/lunchback/LunchBackApplication.java
@@ -0,0 +1,13 @@
+package hello.lunchback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchBackApplication.class, args);
+    }
+
+}
---
+++ lunch-back/src/test/java/hello/lunchback/LunchBackApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.lunchback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
