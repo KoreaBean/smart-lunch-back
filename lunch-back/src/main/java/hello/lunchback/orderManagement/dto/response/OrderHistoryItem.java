04171f590c9532c09e1f6cd83fc53b690978945d
KoreaBean <ksg04001@daum.net>
2024-11-23 23:07:35 +0900

    before master merge

--- lunch-back/src/main/java/hello/lunchback/orderManagement/dto/response/OrderHistoryItem.java
+++ lunch-back/src/main/java/hello/lunchback/orderManagement/dto/response/OrderHistoryItem.java
@@ -1,18 +1,22 @@
 package hello.lunchback.orderManagement.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderHistoryItem {
     private String orderDate;
     private Integer orderId;
     private String storeImage;
     private String storeName;
     private List<String> menuName = new ArrayList<>();
-    private Integer totalPrice;
+    private Integer totalPrice = 0;
     // 주문 날짜, 가게 사진, 가게명, 메뉴, 총 가격
 
 }
