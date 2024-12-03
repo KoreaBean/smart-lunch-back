6540033bfa235a80bfe7344221670ed3495f5e7f
KoreaBean <ksg04001@daum.net>
2024-12-03 19:49:10 +0900

    diet date 타입 Local로 수정

--- lunch-back/src/main/java/hello/lunchback/dietInfoManager/dto/response/Diet.java
+++ lunch-back/src/main/java/hello/lunchback/dietInfoManager/dto/response/Diet.java
@@ -3,6 +3,8 @@ package hello.lunchback.dietInfoManager.dto.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +12,13 @@ import java.util.List;
 @NoArgsConstructor
 public class Diet {
 
-    private String date;
+    private LocalDate date;
     private List<MenuItem> menuItems = new ArrayList<>();
 
+
+    public void setDate(String orderDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDate parse = LocalDate.parse(orderDate, formatter);
+        this.date = parse;
+    }
 }
